3eef490741adb6160ddf306e74a815a823179a16
danielahn <93danielahn@gmail.com>
2019-09-17 09:40:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("JavaScript is so cool. It lets me add text to my page programmatically.")
+    document.querySelector('p').textContent = "This is really cool!"
+})
